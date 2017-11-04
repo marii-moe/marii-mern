d67a9d7b9c8be6c2c6c6dfd2296a8cc513c6881d
marii <marii@marii.moe>
2017-11-04 16:24:55 -0400

    split prod and dev configurations

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,12 @@ const path = require('path');
 
 module.exports= {
   entry: './src/index.js',
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    host: '0.0.0.0',
+    port: 3000
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
